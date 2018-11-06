0cae2c5de76d28c471a764f048da8e52b658cca4
mantegnous <37369193+mantegnous@users.noreply.github.com>
2018-11-06 17:28:14 +0100

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+  <div class="about">
+    <h1>This is an about page XXXXX</h1>
+    <p> When I have auto save on. Let's see if its a problem </p>
+  </div>
+  <div class="body">
+    This is another DIV Oh yes. And then another change.
+  </div>
+  </div>
+</template>
---
+++ src/views/MKTest.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+  <div class="MKTest">
+    <h1>MKTest</h1>
+    <p> If you see this in the browser then I have successfully created a new view </p>
+  </div>
+  </div>
+</template>
